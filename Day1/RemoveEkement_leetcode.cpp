2bd7888464536d3256003cea379bb54efe071e88
Ashirvad kar pathak <59271649+ashirvadkp33@users.noreply.github.com>
2022-03-24 22:56:03 +0530

    Create RemoveEkement_leetcode.cpp

---
+++ Day1/RemoveEkement_leetcode.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/remove-element/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int j=0,k=0;
+        
+        for(int i=0;i<n;i++)
+        {
+                if(nums[i]!=val){
+                    nums[j]=nums[i];
+                    j++;
+                    k++;
+                }
+            else continue;
+        }
+        return k;
+    }
+};
