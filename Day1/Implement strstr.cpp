3bdfad1feeb6be83a5205a5ac8a5adae1dea5b74
Ashirvad kar pathak <59271649+ashirvadkp33@users.noreply.github.com>
2022-03-24 22:49:57 +0530

    Create Implement strstr.cpp

---
+++ Day1/Implement strstr.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+     //Your code here
+     int ans=-1;
+     int n=s.length();
+     int m=x.length();
+     for(int i=0;i<n;i++)
+     {
+         if(s[i]==x[0])
+         {  ans=i;
+            int k=i;
+             for(int j=0;j<m;j++)
+             {
+                 if(s[k]==x[j])
+                 {   k++;
+                     continue;
+                 }
+                 else{
+                    ans=-1;
+                    break;
+                 }
+             }
+         }
+        
+         if(ans!=-1)
+         break;
+     }
+     return ans;
+}
